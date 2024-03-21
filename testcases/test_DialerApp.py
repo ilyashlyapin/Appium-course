5b53e5e288bbc6803eddb08a98dcd49c6b256111
Ilya Shlyapin <ilya.shlyapin@gmail.com>
2024-03-21 15:09:07 +0100

    dialer case

--- testcases/test_DialerApp.py
+++ testcases/test_DialerApp.py
@@ -1,28 +1,53 @@
 import time
 
 from appium import webdriver
-from selenium.webdriver.common.by import By
+from appium.webdriver.common.appiumby import AppiumBy
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.appium_service import AppiumService
+from appium.options.ios import XCUITestOptions
+from appium.options.common import AppiumOptions
+from selenium.webdriver.common.action_chains import ActionChains
 
-desired_caps = dict(
-
-
-    deviceName='Android',
-    platformName='Android',
-    appPackage='com.android.dialer',
-    appActivity='.BBKTwelveKeyDialer'
-
-)
+APPIUM_PORT = 4723
+APPIUM_HOST = '127.0.0.1'
 
 
-driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+def create_android_driver(custom_opts=None):
+    options = UiAutomator2Options()
+    options.platformVersion = '13'
+    options.udid = 'b4c0762b'
+    options.app_package = 'com.google.android.dialer'
+    options.app_activity = 'com.android.dialer.main.impl.MainActivity'
 
-driver.find_element(By.ID,'com.android.dialer:id/one').click()
-driver.find_element(By.ID,'com.android.dialer:id/two').click()
-driver.find_element(By.ID,'com.android.dialer:id/three').click()
-driver.find_element(By.ID,'com.android.dialer:id/five').click()
-
-driver.find_element(By.ID,'com.android.dialer:id/dialButtonSim1').click()
+    if custom_opts is not None:
+        options.load_capabilities(custom_opts)
+    # Appium1 points to http://127.0.0.1:4723/wd/hub by default
+    return webdriver.Remote(f'http://{APPIUM_HOST}:{APPIUM_PORT}', options=options)
 
 
+# options = AppiumOptions()
+# options.platform_name = 'mac'
+# options.automation_name = 'chrome'
+# set_capability API allows to provide any custom option
+# calls to it could be chained
+# options.set_capability('chrome', 'chrome')
+# desired_caps = dict(
+#
+#
+#     deviceName='Android',
+#     platformName='Android',
+#     browserName='Chrome'
+#
+# )
+appium_service = AppiumService()
+appium_service.start()
+
+driver = create_android_driver()
+driver.implicitly_wait(10)
 time.sleep(2)
 driver.quit()
+appium_service.stop()
+"""
+ActionChains class comes with various methods that help to perform complex actions on webelements. 
+You can go for ActionChains class when it becomes difficult to perform actions by a WebDriver. 
+"""
--- testcases/test_webapplication.py
+++ testcases/test_webapplication.py
@@ -1,6 +1,5 @@
 import time
 
-
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.android import UiAutomator2Options
@@ -12,7 +11,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 APPIUM_PORT = 4723
 APPIUM_HOST = '127.0.0.1'
 
-def create_android_driver(custom_opts = None):
+
+def create_android_driver(custom_opts=None):
     options = UiAutomator2Options()
     options.platformVersion = '13'
     options.udid = 'b4c0762b'
@@ -22,8 +22,6 @@ def create_android_driver(custom_opts = None):
         options.load_capabilities(custom_opts)
     # Appium1 points to http://127.0.0.1:4723/wd/hub by default
     return webdriver.Remote(f'http://{APPIUM_HOST}:{APPIUM_PORT}', options=options)
-
-
 
 
 # options = AppiumOptions()
@@ -43,23 +41,27 @@ def create_android_driver(custom_opts = None):
 appium_service = AppiumService()
 appium_service.start()
 
-
 driver = create_android_driver()
 driver.implicitly_wait(10)
 driver.get("http://google.com")
 # print(driver.title)
 # webdriver.find_element(AppiumBy.XPATH,"//*[@name='q']").send_keys("Hello Appium !!!")
-#driver.find_element_by_name("q").send_keys("Hello Appium !!!")
+# driver.find_element_by_name("q").send_keys("Hello Appium !!!")
 element = driver.switch_to.active_element
 driver.scroll(driver.find_element(by=AppiumBy.XPATH, value="//*[@id='S3BnEe']"),
-            driver.find_element(by=AppiumBy.XPATH, value="//*[@id='RP3V5c']"))
+              driver.find_element(by=AppiumBy.XPATH, value="//*[@id='RP3V5c']"))
 action = ActionChains(driver)
 
 driver.find_element(by=AppiumBy.XPATH, value="//*[@id='L2AGLb']").is_enabled()
 cookie_accept_button = driver.find_element(by=AppiumBy.XPATH, value="//*[@id='L2AGLb']")
-action.click(on_element=cookie_accept_button)
-action.perform()
+# making a variable from finding a button
+action.click(on_element=cookie_accept_button)  # defining an action from ActionsChains
+action.perform()  # performing the defined action
 driver.find_element(by=AppiumBy.XPATH, value="//*[@name='q']").send_keys("Hello Appium!!!")
 time.sleep(2)
 driver.quit()
 appium_service.stop()
+"""
+ActionChains class comes with various methods that help to perform complex actions on webelements. 
+You can go for ActionChains class when it becomes difficult to perform actions by a WebDriver. 
+"""
