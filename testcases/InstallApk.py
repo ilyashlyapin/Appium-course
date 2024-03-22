b7e221275bcd9e458507a52ee868fdb86e8124d4
Ilya Shlyapin <ilya.shlyapin@gmail.com>
2024-03-22 15:17:25 +0100

    amazon app cases

--- testcases/InstallApk.py
+++ testcases/InstallApk.py
@@ -7,7 +7,9 @@ from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.appium_service import AppiumService
-from appium.options.ios import XCUITestOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from appium.options.common import AppiumOptions
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -18,8 +20,10 @@ APPIUM_HOST = '127.0.0.1'
 def create_android_driver(custom_opts=None):
     options = UiAutomator2Options()
     options.platformVersion = '11'
-    options.udid = 'emulator-5554'
-    options.app = 'C:\\Users\\User\\OneDrive\\Desktop\\Amazon Shopping_28.6.0.100.apk'
+    options.udid = 'b4c0762b'
+    options.app_package = 'com.amazon.mShop.android.shopping'
+    options.app_activity = 'com.amazon.mShop.home.HomeActivity'
+    options.auto_grant_permissions = True
 
     if custom_opts is not None:
         options.load_capabilities(custom_opts)
@@ -31,14 +35,27 @@ appium_service.start()
 
 driver = create_android_driver()
 driver.implicitly_wait(5)
-# driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='2').click()
-# driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='plus').click()
+# allowButton = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]')
+# allowButton.click()
+# action = ActionChains(driver)
+# action.click(on_element=permission_allow_button)
+# action.perform()
+driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value='new UiSelector().text("Listo")').click()
+# driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@text="Listo"]').click()
+# driver.find_element(by=AppiumBy.ID,value='com.amazon.mShop.android.shopping:id/sso_continue').click()
+# driver.find_element(by=AppiumBy.ID,value='com.amazon.mShop.android.shopping:id/chrome_search_hint_view').click()
+# wait = WebDriverWait(driver,10)
+# wait.until(EC.element_to_be_clickable((By.ID, 'com.amazon.mShop.android.shopping:id/rs_search_src_text')))
+# driver.find_element(by=AppiumBy.ID,value='com.amazon.mShop.android.shopping:id/rs_search_src_text').send_keys('shoes')
+driver.press_keycode(66)
+
+
 # driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='4').click()
 # driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='equals').click()
 # result = driver.find_element(by=AppiumBy.ID, value='com.google.android.calculator:id/result_final').text
 # print(result)
 # assert int(result) == 6
 
-time.sleep(2)
+time.sleep(6)
 driver.quit()
 appium_service.stop()
